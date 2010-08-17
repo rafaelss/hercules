148c14331af568f095588230ead63927550d4aeb
Diogo de Oliveira Biazus <diogob@gmail.com>
2010-08-17 15:51:28 -0300

    Now we copy the checkout to a checkouts directory and we also create a symbolic link in environments folder in GitHandler.

--- src/git_handler.rb
+++ src/git_handler.rb
@@ -1,10 +1,32 @@
 # coding: utf-8
+require 'git'
+
 class GitHandler
   def initialize(options)
     @options = options
   end
 
-  def checkout_to_directory(branch)
-    Git.export(@options['repository'], @options[branch]['target_directory'])
+  def export_branch(branch = 'master')
+    tmp_dir = "#{@options['target_directory']}/checkouts/#{branch}/.tmp"
+    repo = Git.clone(@options['repository'], tmp_dir, {:depth => 1})
+    repo.checkout("origin/#{branch}")
+    commit_dir = "#{@options['target_directory']}/checkouts/#{branch}/#{repo.gcommit('HEAD').sha}"
+    Dir.chdir(repo.dir.to_s) { FileUtils.rm_r '.git' }
+    FileUtils.mv repo.dir.to_s, commit_dir
+    commit_dir
+  end
+
+  def environments_path
+    "#{@options['target_directory']}/environments"
+  end
+
+  def create_environments_dir
+    FileUtils.mkdir_p environments_path
+    environments_path
+  end
+
+  def deploy_branch(branch = 'master')
+    FileUtils.ln_sf(export_branch(branch), "#{create_environments_dir}/#{branch}")
+    self
   end
 end
--- tests/git_handler_test.rb
+++ tests/git_handler_test.rb
@@ -7,7 +7,7 @@ class GitHandlerTest < Test::Unit::TestCase
     @config = YAML.load_file(File.dirname(__FILE__) + '/fixtures/config.yml')
     dir = @config['test_project']['repository'].gsub(/file:\/\//, '')
     FileUtils.rm_rf(dir)
-    FileUtils.rm_rf(@config['test_project']['test']['target_directory'])
+    FileUtils.rm_rf(@config['test_project']['target_directory'])
     FileUtils.mkdir_p(dir)
     FileUtils.cp(File.dirname(__FILE__) + '/fixtures/config.yml', "#{dir}/config.yml")
     g = Git.init(dir)
@@ -16,14 +16,36 @@ class GitHandlerTest < Test::Unit::TestCase
       g.config('user.email', 'email@email.com')
       g.add("./config.yml")
       g.commit_all('message')
+      @head_sha = g.gcommit('HEAD').sha
     end
   end
 
-  def test_checkout_to_directory
+  def assert_is_checkout(dir)
+    assert_equal File.open(File.dirname(__FILE__) + '/fixtures/config.yml').read, File.open(dir + '/config.yml').read
+    assert !File.exists?(dir + '/.git')
+  end
+
+  def test_export_branch
     g = GitHandler.new(@config['test_project'])
-    g.checkout_to_directory('test')
-    assert_equal File.open(File.dirname(__FILE__) + '/fixtures/config.yml').read, File.open(@config['test_project']['test']['target_directory'] + '/config.yml').read
-    assert !File.exists?(@config['test_project']['test']['target_directory'] + '/.git')
+    g.export_branch('master')
+    assert_is_checkout @config['test_project']['target_directory'] + '/checkouts/master/' + @head_sha
+  end
+
+  def test_create_environments_dir
+    g = GitHandler.new(@config['test_project'])
+    g.create_environments_dir
+    assert File.exists? @config['test_project']['target_directory'] + '/environments'
+  end
+
+  def test_environments_dir
+    g = GitHandler.new(@config['test_project'])
+    assert_equal @config['test_project']['target_directory'] + '/environments', g.environments_path
+  end
+
+  def test_deploy_branch
+    g = GitHandler.new(@config['test_project'])
+    g.deploy_branch('master')
+    assert_is_checkout @config['test_project']['target_directory'] + '/environments/master'
   end
 end
 
