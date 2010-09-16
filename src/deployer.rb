fd82b75c725a071eed613ab4f768049133c4fcda
Diogo Biazus <diogob@gmail.com>
2010-09-15 22:31:40 -0300

    removed test code

--- src/deployer.rb
+++ src/deployer.rb
@@ -24,7 +24,6 @@ module Hercules
     def deploy
       git = GitHandler.new @config
       git.deploy_branch(@branch) do |dir, branch|
-        `cd #{dir}`
         @cmd.cd(dir).run!("bundle install --deployment")
         @trigger_class = look_for_triggers(dir)
         before_trigger(dir) if has_before_trigger?
