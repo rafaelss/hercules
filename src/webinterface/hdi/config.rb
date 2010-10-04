b75bf36901f35ba9a3a33e17335ead482f408c55
Leonardo Tartari <ltartari@gmail.com>
2010-10-04 12:12:26 -0300

    mods para layout

--- src/webinterface/hdi/config.rb
+++ src/webinterface/hdi/config.rb
@@ -1,10 +1,7 @@
 ROOT = File.join(File.dirname(__FILE__), '/')
-puts "Site root is: " + File.expand_path(ROOT)
-
+puts "HDI root is at" + File.expand_path(ROOT)
 output_style      = :compressed
-project_path      = ROOT               # must be set for Compass to work 
-sass_dir          = "src/stylesheets"  # dir containing Sass / Compass source files
-http_path         = "/"                # root when deployed
-css_dir           = "site/stylesheets" # final CSS
-images_dir        = "site/images"      # final images
-http_images_path  = "/images"
+project_path      = ROOT                                            # must be set for Compass to work
+sass_dir          = "/usr/lib/ruby/gems/1.8/gems/compass-0.10.5"    # dir containing Sass / Compass source files
+http_path         = "/site/"                                        # root when deployed
+css_dir           = "/src/pages/"                                   # final CSS
